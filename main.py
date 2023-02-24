7b2cd2619ee9f60c40081e309c6bab9bbf730f04
tombjorn <tombjorn.audio@gmail.com>
2023-02-24 00:03:27 +0000

    refactor

--- main.py
+++ main.py
@@ -1,4 +1,45 @@
-def main():
-    print('hello world')
+from modules import *
+import asyncio
+import sys
 
-main()
+def main():
+
+    threshold = float(sys.argv[1])
+    path = str(sys.argv[2])
+
+    print('Generating picture instance list')
+    pictures = generate_pictures(path)
+
+    sharp_m = mean(pictures)
+    sharp_s = std(pictures)
+
+
+    print('Adding colour attribute')
+    for p in pictures:
+        img = p['instance']
+        img.get_col(t=threshold, m=sharp_m, s=sharp_s)
+
+    axes_data = format_axes(pictures)
+
+    print('Files tagging')
+    asyncio.run(tag_files(pictures))
+
+
+    print('Drawing Plot')
+    draw_plot(axes_data, sharp_m)
+
+    
+    print('DONE')
+
+
+# SHOULD DO A REGEX TEST TO MAKE SURE THRESH IS FLOAT AND DIR IS PATH
+if len(sys.argv) == 1:
+    cli_error()
+elif sys.argv[1] == 'help':
+    get_help()
+else:
+    main()
+    
+
+
+# raise ValueError('A very specific bad thing happened.')
